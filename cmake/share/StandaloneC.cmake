8b3766ae9efc8691cab527c401239ea46667e4c0
Nat! <nat@mulle-kybernetik.com>
2020-04-18 23:16:58 +0200

    * new mulle-sde project structure

--- cmake/share/StandaloneC.cmake
+++ cmake/share/StandaloneC.cmake
@@ -31,7 +31,7 @@ if( STANDALONE)
    endif()
 
    if( NOT STANDALONE_DEFINITIONS)
-      set( STANDALONE_DEFINITIONS ${mulle_civet_web_DEFINITIONS})
+      set( STANDALONE_DEFINITIONS ${MULLE_CIVET_WEB_DEFINITIONS})
    endif()
 
    #
